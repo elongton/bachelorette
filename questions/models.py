6a281ee14a760d3b45d6fd5541aa18e0671ce467
Max Longton <elongton@vt.edu>
2017-09-14 08:14:49 -0400

    switched some things in the models

--- questions/models.py
+++ questions/models.py
@@ -3,12 +3,12 @@ from django.core.urlresolvers import reverse
 # Create your models here.
 
 class Place(models.Model):
-    # place_id = models.PositiveIntegerField(primary_key=True)
+    place_id = models.PositiveIntegerField(primary_key=True)
     next_id = models.PositiveIntegerField(default=0)
-    name_of_establishment = models.CharField(max_length=100)
     instructions = models.TextField(default='')
     plank_clue = models.TextField(default='')
     plank_prompt = models.TextField(default='')
+    name_of_establishment = models.CharField(max_length=100)
     plank_answer = models.CharField(max_length=100)
     is_current = models.BooleanField(default=False)
     location_complete = models.BooleanField(default = False)
